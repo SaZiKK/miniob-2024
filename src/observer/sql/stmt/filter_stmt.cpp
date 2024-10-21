ca65f1d594c804372e51e66b1ade33c4dc5c96cb
dmx20070206 <dmx20070206@163.com>
2024-10-21 23:11:40 +0800

    🐞 fix:修复了IN运算符以及非法的TABLE.ATTR的判断

--- src/observer/sql/stmt/filter_stmt.cpp
+++ src/observer/sql/stmt/filter_stmt.cpp
@@ -21,29 +21,34 @@ See the Mulan PSL v2 for more details. */
 #include "storage/table/table.h"
 #include <common/type/date_type.h>
 
-FilterStmt::~FilterStmt() {
-  for (FilterUnit *unit : filter_units_) {
+FilterStmt::~FilterStmt()
+{
+  for (FilterUnit *unit : filter_units_)
+  {
     delete unit;
   }
   filter_units_.clear();
 }
 
 RC FilterStmt::create(Db *db, Table *default_table, std::unordered_map<std::string, Table *> *tables, const ConditionSqlNode *conditions,
-                      int condition_num, FilterStmt *&stmt) {
+                      int condition_num, FilterStmt *&stmt)
+{
   RC rc = RC::SUCCESS;
   stmt = nullptr;
 
   FilterStmt *tmp_stmt = new FilterStmt();
 
   // 遍历全部谓词
-  for (int i = 0; i < condition_num; i++) {
+  for (int i = 0; i < condition_num; i++)
+  {
     FilterUnit *filter_unit = nullptr;
 
     // 创建单个筛选条件
     rc = create_filter_unit(db, default_table, tables, conditions[i], filter_unit);
 
     // 查错
-    if (rc != RC::SUCCESS) {
+    if (rc != RC::SUCCESS)
+    {
       delete tmp_stmt;
       LOG_WARN("failed to create filter unit. condition index=%d", i);
       return rc;
@@ -58,28 +63,37 @@ RC FilterStmt::create(Db *db, Table *default_table, std::unordered_map<std::stri
 }
 
 RC get_table_and_field(Db *db, Table *default_table, std::unordered_map<std::string, Table *> *tables, Expression *expression, Table *&table,
-                       const FieldMeta *&field) {
+                       const FieldMeta *&field)
+{
   UnboundFieldExpr *field_expr = static_cast<UnboundFieldExpr *>(expression);
   string relation_name = field_expr->table_name();
   string attribute_name = field_expr->field_name();
 
-  if (common::is_blank(relation_name.c_str())) {
+  if (common::is_blank(relation_name.c_str()))
+  {
     table = default_table;
-  } else if (nullptr != tables) {
+  }
+  else if (nullptr != tables)
+  {
     auto iter = tables->find(relation_name);
-    if (iter != tables->end()) {
+    if (iter != tables->end())
+    {
       table = iter->second;
     }
-  } else {
+  }
+  else
+  {
     table = db->find_table(relation_name.c_str());
   }
-  if (nullptr == table) {
+  if (nullptr == table)
+  {
     LOG_WARN("No such table: attr.relation_name: %s", relation_name.c_str());
     return RC::SCHEMA_TABLE_NOT_EXIST;
   }
 
   field = table->table_meta().field(attribute_name.c_str());
-  if (nullptr == field) {
+  if (nullptr == field)
+  {
     LOG_WARN("no such field in table: table %s, field %s", table->name(), attribute_name.c_str());
     table = nullptr;
     return RC::SCHEMA_FIELD_NOT_EXIST;
@@ -88,63 +102,94 @@ RC get_table_and_field(Db *db, Table *default_table, std::unordered_map<std::str
   return RC::SUCCESS;
 }
 
-RC FilterStmt::bind_filter_expr(Db *db, Table *default_table, std::unordered_map<std::string, Table *> *tables, unique_ptr<Expression> &expr) {
-  if (expr == nullptr) return RC::INVALID_ARGUMENT;
+RC FilterStmt::bind_filter_expr(Db *db, Table *default_table, std::unordered_map<std::string, Table *> *tables, unique_ptr<Expression> &expr)
+{
+  if (expr == nullptr)
+    return RC::INVALID_ARGUMENT;
 
   /*
-  对于简单的 Value 表达式，不需要绑定，只需要检验日期合法性即可
+  对于简单的 Value ValueList 表达式，不需要绑定，只需要检验日期合法性即可
   对于简单的 SubQuery 表达式，不需要绑定
   对于简单的 UnBoundField 表达式，将其绑定成 Field 表达式
   对于算术表达式，递归绑定左右两侧的表达式
   出现上述四种情况之外的情况均为非法情况
   */
 
-  switch (expr->type()) {
-    case ExprType::VALUE: {
-      Value value;
-      expr->try_get_value(value);
-      if (value.attr_type() == AttrType::DATE && !DateType::check_date(&value)) {
-        return RC::INVALID_ARGUMENT;
-      }
-    } break;
-    case ExprType::SUBQUERY: {
-      vector<vector<Value>> tuple_list;
-      RC rc = expr->get_tuple_list(tuple_list);
-      if (rc != RC::SUCCESS) return rc;
-    } break;
-    case ExprType::UNBOUND_FIELD: {
-      Table *table;
-      const FieldMeta *field;
-      RC rc = get_table_and_field(db, default_table, tables, expr.get(), table, field);
-      if (rc != RC::SUCCESS) return rc;
-      expr = std::make_unique<FieldExpr>(table, field);
-    } break;
-    case ExprType::ARITHMETIC: {
-      RC rc = RC::SUCCESS;
-      ArithmeticExpr *arith_expr = static_cast<ArithmeticExpr *>(expr.get());
-      if (arith_expr->left() != nullptr) {
-        rc = bind_filter_expr(db, default_table, tables, arith_expr->left());
-      }
-      if (rc != RC::SUCCESS) return rc;
-      if (arith_expr->right() != nullptr) {
-        rc = bind_filter_expr(db, default_table, tables, arith_expr->right());
-      }
-      if (rc != RC::SUCCESS) return rc;
-    } break;
-    default:
+  switch (expr->type())
+  {
+  case ExprType::VALUE:
+  {
+    Value value;
+    RC rc = expr->try_get_value(value);
+    if (rc != RC::SUCCESS)
+      return rc;
+    if (value.attr_type() == AttrType::DATE && !DateType::check_date(&value))
       return RC::INVALID_ARGUMENT;
-      break;
+  }
+  break;
+  case ExprType::VALUELIST:
+  {
+    std::vector<Value> value_list;
+    RC rc = expr->get_value_list(value_list);
+    if (rc != RC::SUCCESS)
+      return rc;
+    for (auto it : value_list)
+      if (it.attr_type() == AttrType::DATE && !DateType::check_date(&it))
+        return RC::INVALID_ARGUMENT;
+  }
+  break;
+  case ExprType::SUBQUERY:
+  {
+    vector<vector<Value>> tuple_list;
+    RC rc = expr->get_tuple_list(tuple_list);
+    if (rc != RC::SUCCESS)
+      return rc;
+  }
+  break;
+  case ExprType::UNBOUND_FIELD:
+  {
+    Table *table = nullptr;
+    const FieldMeta *field = nullptr;
+    RC rc = get_table_and_field(db, default_table, tables, expr.get(), table, field);
+    if (rc != RC::SUCCESS)
+      return rc;
+    expr = std::make_unique<FieldExpr>(table, field);
+  }
+  break;
+  case ExprType::ARITHMETIC:
+  {
+    RC rc = RC::SUCCESS;
+    ArithmeticExpr *arith_expr = static_cast<ArithmeticExpr *>(expr.get());
+    if (arith_expr->left() != nullptr)
+    {
+      rc = bind_filter_expr(db, default_table, tables, arith_expr->left());
+    }
+    if (rc != RC::SUCCESS)
+      return rc;
+    if (arith_expr->right() != nullptr)
+    {
+      rc = bind_filter_expr(db, default_table, tables, arith_expr->right());
+    }
+    if (rc != RC::SUCCESS)
+      return rc;
+  }
+  break;
+  default:
+    return RC::INVALID_ARGUMENT;
+    break;
   }
   return RC::SUCCESS;
 }
 
 RC FilterStmt::create_filter_unit(Db *db, Table *default_table, std::unordered_map<std::string, Table *> *tables, const ConditionSqlNode &condition,
-                                  FilterUnit *&filter_unit) {
+                                  FilterUnit *&filter_unit)
+{
   RC rc = RC::SUCCESS;
 
   // 拿到比较符并且判断其合法性
   CompOp comp = condition.comp;
-  if (comp < EQUAL_TO || comp >= NO_OP) {
+  if (comp < EQUAL_TO || comp >= NO_OP)
+  {
     LOG_WARN("invalid compare operator : %d", comp);
     return RC::INVALID_ARGUMENT;
   }
@@ -155,14 +200,18 @@ RC FilterStmt::create_filter_unit(Db *db, Table *default_table, std::unordered_m
   std::unique_ptr<Expression> right_expr(condition.right_expression);
 
   // 构造子查询表达式
-  if (condition.left_is_sub_query) left_expr = make_unique<SubQueryExpr>(condition.left_sub_query_stmt);
-  if (condition.right_is_sub_query) right_expr = make_unique<SubQueryExpr>(condition.right_sub_query_stmt);
+  if (condition.left_is_sub_query)
+    left_expr = make_unique<SubQueryExpr>(condition.left_sub_query_stmt);
+  if (condition.right_is_sub_query)
+    right_expr = make_unique<SubQueryExpr>(condition.right_sub_query_stmt);
 
   // 绑定表达式
   rc = bind_filter_expr(db, default_table, tables, left_expr);
-  if (rc != RC::SUCCESS) return rc;
+  if (rc != RC::SUCCESS)
+    return rc;
   rc = bind_filter_expr(db, default_table, tables, right_expr);
-  if (rc != RC::SUCCESS) return rc;
+  if (rc != RC::SUCCESS)
+    return rc;
 
   FilterObj left_obj;
   left_obj.init(std::move(left_expr));
