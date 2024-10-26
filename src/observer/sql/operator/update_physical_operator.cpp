bfe8e66d4a137f144e3fa8e4e543cfbdb41cac38
SaZiKK <ck_look@outlook.com>
2024-10-27 03:12:42 +0800

    🐞 fix: add table level recovery for update failure

--- src/observer/sql/operator/update_physical_operator.cpp
+++ src/observer/sql/operator/update_physical_operator.cpp
@@ -35,13 +35,36 @@ RC UpdatePhysicalOperator::open(Trx *trx) {
     LOG_WARN("no records to update");
   }
 
+  std::vector<char *> backup_datas;
+  for (Record &record : records_) {
+    Record backup_record;
+    char *old_data = record.data();
+    char *backup_data = (char *)malloc(record.len());
+    memcpy(backup_data, old_data, record.len());
+
+    backup_datas.push_back(backup_data);
+  }
+
+  int update_num = 0;
+
   // 先收集记录再更新
   // 记录的有效性由事务来保证，如果事务不保证更新的有效性，那说明此事务类型不支持并发控制，比如VacuousTrx
   for (Record &record : records_) {
     rc = trx_->update_records(table_, record, update_map_);
     if (rc != RC::SUCCESS) {
+      // 如果更新失败，需要回滚
+      assert(backup_datas.size() == records_.size());  // 确保两个 vector 长度相等
+      for (size_t i = 0; i < update_num; ++i) {
+        char *backup_data = backup_datas[i];
+        Record &record = records_[i];
+
+        record.set_data(backup_data);
+        table_->record_handler()->update_record(&record);
+      }
+      LOG_WARN("failed to update record, recovering. table=%s, rc=%d:%s", table_->name(), rc, strrc(rc));
       return rc;
     }
+    ++update_num;
   }
 
   return RC::SUCCESS;
