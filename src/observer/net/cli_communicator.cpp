e4e10a6ea0de29e46ddc2aa4687184db65bcdb34
SaZiKK <ck_look@outlook.com>
2024-11-01 00:00:47 +0800

    ✨ feat: 是缓冲区不够大aminoac

--- src/observer/net/cli_communicator.cpp
+++ src/observer/net/cli_communicator.cpp
@@ -28,7 +28,7 @@ See the Mulan PSL v2 for more details. */
 #include "readline/readline.h"
 #endif
 
-#define MAX_MEM_BUFFER_SIZE 8192
+#define MAX_MEM_BUFFER_SIZE 8192 * 8 + 4096
 #define PORT_DEFAULT 6789
 
 using namespace common;
