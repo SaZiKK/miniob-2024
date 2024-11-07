6443ac46545e33dbbf5f4d4420025e8c3d37cd28
SaZiKK <ck_look@outlook.com>
2024-11-07 09:20:24 +0800

    ✨ feat: add virtual base class for table/view

--- src/observer/storage/table/base_table.h
+++ src/observer/storage/table/base_table.h
@@ -18,7 +18,6 @@ See the Mulan PSL v2 for more details. */
 #include "common/types.h"
 #include "common/lang/span.h"
 #include "common/lang/functional.h"
-#include "storage/table/base_table.h"
 
 struct RID;
 class Record;
@@ -35,32 +34,31 @@ class Trx;
 class Db;
 
 /**
- * @brief 表
+ * @brief 视图和表的基类
  *
  */
-class Table : public BaseTable {
-  friend class Index;
+class BaseTable {
  public:
-  Table() = default;
-  ~Table();
+  BaseTable() = default;
+  ~BaseTable();
 
   /**
-   * 创建一个表
+   * 创建一个表/视图
    * @param path 元数据保存的文件(完整路径)
    * @param name 表名
    * @param base_dir 表数据存放的路径
    * @param attribute_count 字段个数
    * @param attributes 字段
    */
-  RC create(Db *db, int32_t table_id, const char *path, const char *name, const char *base_dir, span<const AttrInfoSqlNode> attributes,
-            StorageFormat storage_format);
+  virtual RC create(Db *db, int32_t table_id, const char *path, const char *name, const char *base_dir, span<const AttrInfoSqlNode> attributes,
+            StorageFormat storage_format) { return RC::UNIMPLEMENTED; }
 
   /**
-   * 打开一个表
+   * 打开一个表/视图
    * @param meta_file 保存表元数据的文件完整路径
    * @param base_dir 表所在的文件夹，表记录数据文件、索引数据文件存放位置
    */
-  RC open(Db *db, const char *meta_file, const char *base_dir);
+  virtual RC open(Db *db, const char *meta_file, const char *base_dir) { return RC::UNIMPLEMENTED; }
 
   /**
    * @brief 根据给定的字段生成一个记录/行，检查字段类型，是否为NULL等，并复制数据到record中
@@ -69,7 +67,7 @@ class Table : public BaseTable {
    * @param values    每个字段的值
    * @param record    生成的记录数据
    */
-  RC make_record(int value_num, const Value *values, Record &record);
+  virtual RC make_record(int value_num, const Value *values, Record &record) { return RC::UNIMPLEMENTED; }
 
   /**
    * @brief 在当前的表中插入一条记录
@@ -77,26 +75,26 @@ class Table : public BaseTable {
    * 在表文件和索引中插入关联数据。这里只管在表中插入数据，不关心事务相关操作。
    * @param record[in/out] 传入的数据包含具体的数据，插入成功会通过此字段返回RID
    */
-  RC insert_record(Record &record);
-  RC delete_record(const Record &record);
-  RC delete_record(const RID &rid);
-  RC update_record(Record &record, const char *attr_name, Value *value);
-  RC update_records(Record &record, std::vector<std::pair<Value, FieldMeta>> update_map_);
-  RC get_record(const RID &rid, Record &record);
+  virtual RC insert_record(Record &record);
+  virtual RC delete_record(const Record &record);
+  virtual RC delete_record(const RID &rid);
+  virtual RC update_record(Record &record, const char *attr_name, Value *value);
+  virtual RC update_records(Record &record, std::vector<std::pair<Value, FieldMeta>> update_map_);
+  virtual RC get_record(const RID &rid, Record &record);
 
   // 删除表格函数
-  RC drop();
+  virtual RC drop();
 
-  RC recover_insert_record(Record &record);
+  virtual RC recover_insert_record(Record &record);
 
   // TODO refactor
-  RC create_index(Trx *trx, const std::vector<FieldMeta> &fieldmetas, const char *index_name, bool is_unique);
+  virtual RC create_index(Trx *trx, const std::vector<FieldMeta> &fieldmetas, const char *index_name, bool is_unique);
 
-  RC get_record_scanner(RecordFileScanner &scanner, Trx *trx, ReadWriteMode mode);
+  virtual RC get_record_scanner(RecordFileScanner &scanner, Trx *trx, ReadWriteMode mode);
 
-  RC get_chunk_scanner(ChunkFileScanner &scanner, Trx *trx, ReadWriteMode mode);
+  virtual RC get_chunk_scanner(ChunkFileScanner &scanner, Trx *trx, ReadWriteMode mode);
 
-  RecordFileHandler *record_handler() const { return record_handler_; }
+  virtual RecordFileHandler *record_handler() const = 0;
 
   /**
    * @brief 可以在页面锁保护的情况下访问记录
@@ -105,38 +103,30 @@ class Table : public BaseTable {
    * @param visitor
    * @return RC
    */
-  RC visit_record(const RID &rid, function<bool(Record &)> visitor);
+  virtual RC visit_record(const RID &rid, function<bool(Record &)> visitor);
 
  public:
-  int32_t table_id() const { return table_meta_.table_id(); }
-  const char *name() const;
+  virtual int32_t table_id() const = 0;
+  virtual const char *name() const;
 
-  Db *db() const { return db_; }
+  virtual Db *db() const = 0;
 
-  const TableMeta &table_meta() const;
+  virtual const TableMeta &table_meta() const;
 
-  RC sync();
+  virtual RC sync();
 
-  DiskBufferPool *data_buffer_pool() const { return data_buffer_pool_; }
-
- private:
-  RC insert_entry_of_indexes(const char *record, const RID &rid);
-  RC delete_entry_of_indexes(const char *record, const RID &rid, bool error_on_not_exists);
-  RC set_value_to_record(char *record_data, Value &value, const FieldMeta *field, int index);
+  virtual DiskBufferPool *data_buffer_pool() const = 0;
 
  private:
-  RC init_record_handler(const char *base_dir);
+  virtual RC insert_entry_of_indexes(const char *record, const RID &rid);
+  virtual RC delete_entry_of_indexes(const char *record, const RID &rid, bool error_on_not_exists);
+  virtual RC set_value_to_record(char *record_data, Value &value, const FieldMeta *field, int index);
+
+ private:
+  virtual RC init_record_handler(const char *base_dir);
 
  public:
-  Index *find_index(const char *index_name) const;
-  Index *find_index_by_field(const char *field_name) const;
-  Index *find_index_by_fields(const std::vector<const char *> field_name) const;
-
- private:
-  Db *db_ = nullptr;
-  string base_dir_;
-  TableMeta table_meta_;
-  DiskBufferPool *data_buffer_pool_ = nullptr;   /// 数据文件关联的buffer pool
-  RecordFileHandler *record_handler_ = nullptr;  /// 记录操作
-  vector<Index *> indexes_;
+  virtual Index *find_index(const char *index_name) const;
+  virtual Index *find_index_by_field(const char *field_name) const;
+  virtual Index *find_index_by_fields(const std::vector<const char *> field_name) const;
 };
